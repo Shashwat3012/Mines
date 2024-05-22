62ee13910e7b29b4afc74b647913ceca8497ba86
Piyush Tilokani <2021.piyush.tilokani@ves.ac.in>
2024-05-23 01:03:16 +0530

    Improve UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,16 @@
 import "./styles/App.css";
 import Board from "./Board";
+import SideBar from "./SideBar";
 
 function App() {
   return (
     <>
-      <div className="App">
-        <h1>Mines</h1>
-        <Board />
+      <div className="App bg-black ">
+        <h1 className="text-white text-4xl">Mines</h1>
+        <div className="flex flex-row">
+          <SideBar />
+          <Board />
+        </div>
       </div>
     </>
   );
